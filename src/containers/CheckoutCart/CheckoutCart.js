fe9ce1aa070bc3a58cca64cd6657aaee0440d9ef
endmaster0809 <endmaster0809@outlook.com>
2020-07-17 14:25:53 +0200

    Add checkout cart component

---
+++ src/containers/CheckoutCart/CheckoutCart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, CheckoutButton } from './CheckoutCart.style';
+
+const CheckoutCart = () => {
+  return (
+    <Container>
+      <CheckoutButton>Checkout</CheckoutButton>
+    </Container>
+  );
+};
+
+export default CheckoutCart;
---
+++ src/containers/CheckoutCart/CheckoutCart.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+const CheckoutButton = styled.button``;
+
+export { Container, CheckoutButton };
