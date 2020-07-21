b33c55a8a5186aa950bee5ee38f3def15ef31d86
endmaster0809 <endmaster0809@outlook.com>
2020-07-21 18:19:48 +0200

    Update app grid values

--- src/App.style.js
+++ src/App.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: 30% 70%;
+  grid-template-columns: 25% 75%;
   overflow-y: auto;
   height: calc(100vh - 70px);
 `;
