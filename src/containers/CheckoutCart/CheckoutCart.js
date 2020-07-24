67986b4c3a7142ae888dbf35020ec891e50ed05a
endmaster0809 <endmaster0809@outlook.com>
2020-07-24 10:19:48 +0200

    Add checkout item component

--- src/containers/CheckoutCart/CheckoutCart.js
+++ src/containers/CheckoutCart/CheckoutCart.js
@@ -1,9 +1,56 @@
 import React from 'react';
-import { Container, CheckoutButton } from './CheckoutCart.style';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Container,
+  Heading,
+  CheckoutButton,
+  CloseCheckoutIcon,
+  SubtotalContainer,
+  SubtotalTitle,
+  Subtotal,
+} from './CheckoutCart.style';
+import CartStatus from '../CartStatus';
+import closeCheckoutIcon from '../../assets/closeCheckout.svg';
+import { closeCheckout } from '../../redux/actions/checkoutActions';
+import CheckoutItem from '../CheckoutItem';
 
 const CheckoutCart = () => {
+  const dispatch = useDispatch();
+  const checkoutItems = useSelector((state) => state.checkout.items);
+
+  const subtotal = Object.keys(checkoutItems).reduce(
+    (total, current) => checkoutItems[current].workshopSubtotal + total,
+    0
+  );
+
+  const closeCheckoutHandler = () => dispatch(closeCheckout());
+
   return (
     <Container>
+      <Heading>
+        <CartStatus />
+        <CloseCheckoutIcon
+          alt='close checkout'
+          src={closeCheckoutIcon}
+          onClick={closeCheckoutHandler}
+        />
+      </Heading>
+      {Object.keys(checkoutItems).map((item) => (
+        <CheckoutItem
+          key={checkoutItems[item].id}
+          id={checkoutItems[item].id}
+          title={checkoutItems[item].title}
+          price={checkoutItems[item].price}
+          imageUrl={checkoutItems[item].imageUrl}
+          numberOfTickets={checkoutItems[item].numberOfTickets}
+        />
+      ))}
+      <SubtotalContainer>
+        <SubtotalTitle>Subtotal</SubtotalTitle>
+        <Subtotal>
+          <span>{subtotal}</span> EUR
+        </Subtotal>
+      </SubtotalContainer>
       <CheckoutButton>Checkout</CheckoutButton>
     </Container>
   );
