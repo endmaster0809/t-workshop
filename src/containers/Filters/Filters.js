5a4e62b83f2e63d58d649716f9db65a0aeb80284
endmaster0809 <endmaster0809@outlook.com>
2020-07-24 16:57:17 +0200

    Switch filters component to the containers folder

--- src/containers/Filters/Filters.js
+++ src/containers/Filters/Filters.js
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Container, Title, FilterList } from './Filters.style';
-import Filter from '../Filter';
+import Filter from '../../components/Filter';
 import applyFilter from '../../redux/actions/filterActions';
 
 const Filters = () => {
